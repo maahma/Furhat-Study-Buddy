8cdf0fc453e8397ba15fa19507370506d04e3026
Maaha <awkcodergirl@gmail.com>
2024-07-19 18:06:40 +0100

    Setting up the MERN application

---
+++ back-end/App.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = 6005;
+require("./db/connection")
+
+app.use(cors({
+    origin: "http://localhost:3001/",
+    methods:"GET,POST,PUT,DELETE",
+    credentials: true
+}));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).json("Start server")
+});
+
+app.listen(PORT, () => {
+    console.log("Listening on port", PORT)
+})
---
+++ back-end/db/connection.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+const DB = process.env.DATABASE;
+
+mongoose.connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}).then(() => console.log("database connected")).catch((err) => console.log("Error connecting to database: ", err))
